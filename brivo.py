a0ba88e5587245335487c74cbbd77edf3fa4d996
Bob Follek <bfollek@gmail.com>
2019-07-05 17:45:04 -0400

    brivo.users is working ATP

--- brivo.py
+++ brivo.py
@@ -1,22 +1,39 @@
 from oauthlib.oauth2 import LegacyApplicationClient
 from requests_oauthlib import OAuth2Session
+import requests
 
 TOKEN_URL = "https://auth.brivo.com/oauth/token"
+USERS_URL = "https://api.brivo.com/v1/api/users"
 
 
 class Brivo:
 
     def __init__(self, username, password, client_id, client_secret, api_key):
-        self._username = username
-        self._password = password
-        self._client_id = client_id
-        self._client_secret = client_secret
-        self._api_key = api_key
+        # Connect and get an access token
+        oauth = OAuth2Session(
+            client=LegacyApplicationClient(client_id=client_id))
+        self._token = oauth.fetch_token(token_url=TOKEN_URL, username=username,
+                                        password=password, client_id=client_id, client_secret=client_secret)
+        # Set up the headers we'll need for each request
+        self._request_headers = {"api-key": api_key,
+                                 "Authorization": f"bearer {self._token['access_token']}",
+                                 "Content-type": "application/json"}
 
     @property
-    def token(self):
-        oauth = OAuth2Session(
-            client=LegacyApplicationClient(client_id=self._client_id))
-        token = oauth.fetch_token(token_url=TOKEN_URL, username=self._username,
-                                  password=self._password, client_id=self._client_id, client_secret=self._client_secret)
-        return token
+    def users(self, **kwargs):
+        """
+        Return the list of users as a list of dictionaries.
+        Use kwargs to pass any of the optional querystring params.
+        TODO Use the kwargs
+        """
+        r = requests.get(USERS_URL, headers=self._request_headers)
+        r.raise_for_status()
+        return r.json()['data']
+
+
+"""
+Calls to api.brivo.com require a Mashery API key and Authorization header.
+Header 	Value
+api-key 	API_KEY
+Authorization 	bearer ACCESS_TOKEN_VALUE
+"""
--- test_brivo.py
+++ test_brivo.py
@@ -2,16 +2,23 @@ import os
 
 from brivo import Brivo
 
-USERNAME = os.environ["BRIVO_USERNAME"]
-PASSWORD = os.environ["BRIVO_PASSWORD"]
-CLIENT_ID = os.environ["BRIVO_CLIENT_ID"]
-CLIENT_SECRET = os.environ["BRIVO_CLIENT_SECRET"]
+USERNAME = os.environ["BRIVO_SANDBOX_USERNAME"]
+PASSWORD = os.environ["BRIVO_SANDBOX_PASSWORD"]
+CLIENT_ID = os.environ["BRIVO_SANDBOX_CLIENT_ID"]
+CLIENT_SECRET = os.environ["BRIVO_SANDBOX_CLIENT_SECRET"]
 API_KEY = os.environ["BRIVO_API_KEY"]
 
 
-def test_brivo_token():
+def test_token():
     brivo = Brivo(username=USERNAME, password=PASSWORD, client_id=CLIENT_ID,
                   client_secret=CLIENT_SECRET, api_key=API_KEY)
-    token = brivo.token
     # Make sure we can get reasonable data out of the token
-    assert int(token["expires_at"]) > 1562288222
+    assert int(brivo._token["expires_at"]) > 1562288222
+
+
+def test_users():
+    brivo = Brivo(username=USERNAME, password=PASSWORD, client_id=CLIENT_ID,
+                  client_secret=CLIENT_SECRET, api_key=API_KEY)
+    users = brivo.users
+    # I created users in the sandbox with last names of "Example" and "Test"
+    assert [u["lastName"] for u in users] == ["Example", "Test"]
